68a1bdc34a71f048b19c04b1920b2b3cbaae2d78
Trevor <ha.trevor@gmail.com>
2017-01-15 21:51:24 -0500

    minor commit

--- js/submitform.js
+++ js/submitform.js
@@ -1,8 +1,3 @@
 $( document ).ready(function() {
-<<<<<<< HEAD
-    $("#contents").load('php/fetchdata.php');
-=======
-    console.log( "document loaded" );
     $("#contents").load('/php/fetchdata.php');
->>>>>>> master
 });
